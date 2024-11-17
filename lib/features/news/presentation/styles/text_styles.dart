abd90fcab179c7ca6858619244871ee4130450e9
Park Junwoo <boksam1017@gmail.com>
2024-11-17 15:53:59 -0800

    introduce global text styles

---
+++ lib/features/news/presentation/styles/text_styles.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class NewsTextStyles {
+  static const TextStyle headline = TextStyle(
+    fontSize: 24,
+    height: 1.5,
+    fontWeight: FontWeight.w500,
+    color: Colors.white,
+    decoration: TextDecoration.none,
+    letterSpacing: 1,
+    wordSpacing: 1,
+  );
+
+  static const TextStyle body = TextStyle(
+    fontSize: 16,
+    height: 1.5,
+    fontWeight: FontWeight.w300,
+    color: Colors.white,
+    decoration: TextDecoration.none,
+    letterSpacing: 1,
+    wordSpacing: 1,
+  );
+}
