307740fde273961c5c0b108502ee0b2bd011d3f3
Park Junwoo <boksam1017@gmail.com>
2024-11-20 23:29:58 -0800

    add ApiService to help Api call

--- lib/core/config/api_config.dart
+++ lib/core/config/api_config.dart
@@ -1,10 +1,35 @@
+import 'package:http/http.dart' as http;
+
 class ApiConfig {
   static const development = {'baseUrl': 'localhost:3000'};
 
   static const current = development;
+  static String get baseUrl => current['baseUrl']!;
 }
 
 class ApiEndPoints {
   static String articles(DateTime date) => '/api/articles';
   static String articlesById(int id) => '/api/articles/$id';
+}
+
+class ApiService {
+  final String baseUrl;
+  ApiService({String? baseUrl}) : baseUrl = baseUrl ?? ApiConfig.baseUrl;
+
+  Uri getUri(String endpoint, [Map<String, dynamic>? queryParams]) {
+    final uri = Uri.parse('$baseUrl$endpoint');
+
+    if (queryParams == null) return uri;
+
+    uri.replace(
+        queryParameters:
+            queryParams.map((key, value) => MapEntry(key, value.toString())));
+    return uri;
+  }
+
+  Future<http.Response> get(String endpoint,
+      [Map<String, dynamic>? queryParams]) async {
+    final uri = getUri(endpoint);
+    return await http.get(uri);
+  }
 }
